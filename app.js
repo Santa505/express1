eace70009c999ed33f0bac6b9469a05507dd41d2
jeff indall <jeffindall@gmail.com>
2018-06-05 15:29:54 -0600

    passing commit

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(morgan('tiny'));
 
 
 
-function findById(data, id){
-    for (let i = 0; i < data.length; i++){
-        if (data[i].id == id){
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
             return data[i];
         }
     }
@@ -20,24 +20,32 @@ function findById(data, id){
 
 app.use(cors());
 
-
-app.listen(port, () => {
-    app.get("/", function (request, response) {
-        response.json({data: cohorts});
+app.get("/", function (request, response) {
+    response.json({
+        data: cohorts
     });
-    
-    app.get("/:id", function (request, response) {
-        var record = findById(cohorts, request.params.id);
-        if (!record){
-            response.status(404).json({
-                error: {
-                    message: "No record found!"
-                }
-            });
-        } else {
-    
-    response.json({data: record});
-  
+});
+
+app.get("/:id", function (request, response) {
+    var record = findById(cohorts, request.params.id);
+    if (!record) {
+        response.status(404).json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    } else {
+
+        response.json({
+            data: record
+        });
+
     }
 })
+
+app.listen(port, () => {
+
+    console.log("server running")
+
+
 })
